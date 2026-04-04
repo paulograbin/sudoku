a2ed7e101e0b5266daa4b394f903eed1f37e3d78
Paulo Grabin <plgrabin@gmail.com>
2026-04-04 13:07:43 -0300

    Add nightmare level test

--- src/test/java/com/paulograbin/sudoku/SodokuSolverIntegrationTests.java
+++ src/test/java/com/paulograbin/sudoku/SodokuSolverIntegrationTests.java
@@ -57,7 +57,6 @@ public class SodokuSolverIntegrationTests {
         Assertions.assertThat(board.isSolved()).isTrue();
     }
 
-
     @Test
     public void hard() {
         int[][] game = new int[][] {
@@ -79,6 +78,30 @@ public class SodokuSolverIntegrationTests {
         solver.setBoard(board);
         solver.solve();
 
-        Assertions.assertThat(board.isSolved()).isTrue();
+        Assertions.assertThat(board.isSolved()).isFalse();
+    }
+
+    @Test
+    public void nightmare() {
+        int[][] game = new int[][] {
+//               0, 1, 2,   3, 4, 5,   6, 7, 8
+                {0, 0, 0,   5, 9, 6,   0, 0, 0}, // 0
+                {7, 0, 0,   3, 0, 0,   0, 0, 0}, // 1
+                {3, 2, 0,   8, 0, 0,   0, 0, 0}, // 2
+
+                {0, 0, 0,   0, 0, 1,   3, 0, 0}, // 3
+                {4, 0, 1,   0, 0, 0,   0, 0, 2}, // 4
+                {0, 0, 0,   0, 6, 0,   0, 0, 8}, // 5
+
+                {0, 0, 3,   0, 0, 0,   5, 9, 0}, // 6
+                {0, 5, 0,   0, 0, 8,   6, 0, 0}, // 7
+                {0, 9, 0,   0, 0, 0,   0, 0, 0}  // 8
+        };
+
+        SudokuBoard board = new SudokuBoard(game);
+        solver.setBoard(board);
+        solver.solve();
+
+        Assertions.assertThat(board.isSolved()).isFalse();
     }
 }
