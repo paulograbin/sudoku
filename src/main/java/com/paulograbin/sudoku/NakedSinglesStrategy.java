f3c99135494859ac078f8a8c201955c454940d19
Paulo Grabin <plgrabin@gmail.com>
2026-04-04 15:23:09 -0300

    Add log to naked singles strategy

--- src/main/java/com/paulograbin/sudoku/NakedSinglesStrategy.java
+++ src/main/java/com/paulograbin/sudoku/NakedSinglesStrategy.java
@@ -19,13 +19,20 @@ public class NakedSinglesStrategy implements SolvingStrategy {
         return madeProgress;
     }
 
-    private boolean solveCell(SudokuBoard board, int row, int col) {
-        int candidates = board.computeCandidates(row, col);
+    private boolean solveCell(SudokuBoard board, int row, int column) {
+        int candidates = board.computeCandidates(row, column);
+
+        String bitmask = String.format("%9s", Integer.toBinaryString(candidates)).replace(' ', '0');
 
         if (Integer.bitCount(candidates) == 1) {
             int value = Integer.numberOfTrailingZeros(candidates) + 1;
-            board.setValueAt(value, row, col);
+            board.setValueAt(value, row, column);
+
+            System.out.println("Found value for row: " + row + " column: " + column + " are: " + bitmask + "/" + Integer.toBinaryString(candidates) + ", value " + (Integer.numberOfTrailingZeros(candidates) + 1));
+
             return true;
+        } else {
+//            System.out.println("There are " + Integer.bitCount(candidates) + " Candidates for row: " + row + " column: " + column + " are: " + bitmask + "/" + Integer.toBinaryString(candidates));
         }
 
         return false;
