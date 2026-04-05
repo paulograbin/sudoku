6c9d65e66d7407bf26662942ec20905f79145c31
Paulo Grabin <plgrabin@gmail.com>
2026-04-05 16:45:20 -0300

    Extract board config so it can be reused

--- src/test/java/com/paulograbin/sudoku/HiddenSinglesStrategyTest.java
+++ src/test/java/com/paulograbin/sudoku/HiddenSinglesStrategyTest.java
@@ -1,8 +1,10 @@
 package com.paulograbin.sudoku;
 
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.paulograbin.sudoku.BoardRepository.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HiddenSinglesStrategyTest {
@@ -97,5 +99,48 @@ public class HiddenSinglesStrategyTest {
         boolean result = strategy.apply(board);
 
         assertFalse(result);
+    }
+
+    @Test
+    void test() {
+        SudokuBoard board = new SudokuBoard(EASY_GAME);
+
+        while (!board.isSolved()) {
+            boolean apply = strategy.apply(board);
+
+            if (!apply) {
+                Assertions.fail("This strategy was not able to solve the board");
+            }
+        }
+
+        assertThat(board.isSolved()).isTrue();
+    }
+
+    @Test
+    void hard() {
+        SudokuBoard board = new SudokuBoard(HARD_GAME);
+
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            keepGoing = strategy.apply(board);
+        }
+
+        board.printBoard();
+        assertThat(board.isSolved()).isFalse();
+    }
+
+    @Test
+    void nightmare() {
+        SudokuBoard board = new SudokuBoard(NIGHTMARE_GAME);
+
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            keepGoing = strategy.apply(board);
+        }
+
+        board.printBoard();
+        assertThat(board.isSolved()).isFalse();
     }
 }
--- src/test/java/com/paulograbin/sudoku/SodokuSolverIntegrationTests.java
+++ src/test/java/com/paulograbin/sudoku/SodokuSolverIntegrationTests.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static com.paulograbin.sudoku.BoardRepository.*;
+
 
 public class SodokuSolverIntegrationTests {
 
@@ -84,22 +86,7 @@ public class SodokuSolverIntegrationTests {
 
     @Test
     public void easy() {
-        int[][] game = new int[][] {
-//               0, 1, 2,   3, 4, 5,   6, 7, 8
-                {0, 0, 7,   5, 0, 0,   6, 0, 3}, // 0
-                {4, 3, 0,   0, 0, 6,   0, 0, 5}, // 1
-                {6, 0, 8,   1, 0, 9,   0, 2, 7}, // 2
-
-                {2, 0, 6,   4, 5, 0,   0, 0, 0}, // 3
-                {0, 0, 1,   0, 6, 0,   3, 4, 0}, // 4
-                {7, 0, 0,   0, 0, 8,   0, 5, 0}, // 5
-
-                {8, 0, 0,   7, 0, 0,   1, 3, 0}, // 6
-                {0, 7, 4,   0, 2, 0,   5, 9, 0}, // 7
-                {1, 0, 9,   3, 0, 5,   0, 0, 0}  // 8
-        };
-
-        SudokuBoard board = new SudokuBoard(game);
+        SudokuBoard board = new SudokuBoard(EASY_GAME);
         var solver = new SudokuSolver(board);
         solver.solve();
 
@@ -108,22 +95,7 @@ public class SodokuSolverIntegrationTests {
 
     @Test
     public void hard() {
-        int[][] game = new int[][] {
-//               0, 1, 2,   3, 4, 5,   6, 7, 8
-                {0, 0, 2,   0, 0, 0,   0, 0, 0}, // 0
-                {0, 9, 8,   1, 0, 0,   7, 3, 4}, // 1
-                {0, 0, 0,   5, 0, 0,   2, 0, 9}, // 2
-
-                {0, 0, 0,   0, 0, 7,   3, 4, 0}, // 3
-                {0, 2, 7,   4, 1, 0,   5, 0, 0}, // 4
-                {0, 8, 0,   0, 0, 4,   0, 0, 0}, // 5
-
-                {0, 7, 0,   3, 4, 5,   0, 2, 0}, // 6
-                {0, 4, 9,   7, 0, 6,   0, 0, 1}, // 7
-                {0, 0, 0,   0, 0, 0,   0, 0, 0}  // 8
-        };
-
-        SudokuBoard board = new SudokuBoard(game);
+        SudokuBoard board = new SudokuBoard(HARD_GAME);
         var solver = new SudokuSolver(board);
         solver.solve();
 
@@ -132,22 +104,7 @@ public class SodokuSolverIntegrationTests {
 
     @Test
     public void nightmare() {
-        int[][] game = new int[][] {
-//               0, 1, 2,   3, 4, 5,   6, 7, 8
-                {0, 0, 0,   5, 9, 6,   0, 0, 0}, // 0
-                {7, 0, 0,   3, 0, 0,   0, 0, 0}, // 1
-                {3, 2, 0,   8, 0, 0,   0, 0, 0}, // 2
-
-                {0, 0, 0,   0, 0, 1,   3, 0, 0}, // 3
-                {4, 0, 1,   0, 0, 0,   0, 0, 2}, // 4
-                {0, 0, 0,   0, 6, 0,   0, 0, 8}, // 5
-
-                {0, 0, 3,   0, 0, 0,   5, 9, 0}, // 6
-                {0, 5, 0,   0, 0, 8,   6, 0, 0}, // 7
-                {0, 9, 0,   0, 0, 0,   0, 0, 0}  // 8
-        };
-
-        SudokuBoard board = new SudokuBoard(game);
+        SudokuBoard board = new SudokuBoard(NIGHTMARE_GAME);
         var solver = new SudokuSolver(board);
         solver.solve();
 
